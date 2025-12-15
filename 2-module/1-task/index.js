5ff77a02050daf824440868f515b414cbe7d72a9
existmodel <sarizava@yandex.ru>
2025-12-15 23:06:26 +0100

    add fix for 2-module/1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,6 +2,9 @@ function sumSalary(salaries) {
   var sum = 0;
   var value = 0;
   for (let key in salaries) {
+    if ((salaries[key] = null)) {
+      return 0;
+    }
     if (Number.isFinite(salaries[key])) {
       value = salaries[key];
       sum = sum + value;
