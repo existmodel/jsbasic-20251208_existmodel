69126c376f537ed59fe45e5ab5a06695abd22566
existmodel <sarizava@yandex.ru>
2026-01-03 12:27:33 +0100

    add solution for 4-module/1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let friendsLines = friends.map(
+    (user) => `${user.firstName} ${user.lastName}`
+  );
+
+  var list = document.createElement("ul");
+  for (let i = 0; i < friendsLines.length; i++) {
+    var item = document.createElement("li");
+    item.textContent = friendsLines[i];
+    list.append(item);
+  }
+  return list;
 }
