7243fbb480a90d3f5eee7b912b62cf1bdf56b001
existmodel <sarizava@yandex.ru>
2026-01-04 16:01:33 +0100

    add solution for 4-module/3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  let cells = table.querySelectorAll("td");
+  let lastCells = table.querySelectorAll("tr td:last-child");
+  for (let cell of cells) {
+    if (cell.textContent === "m") {
+      cell.parentElement.classList.add("male");
+    } else if (cell.textContent === "f") {
+      cell.parentElement.classList.add("female");
+    }
+    if (cell.textContent < 18) {
+      cell.parentElement.style.textDecoration = "line-through";
+    }
+  }
+  for (let lastCell of lastCells) {
+    if (!lastCell.hasAttribute("data-available")) {
+      lastCell.parentElement.hidden = true;
+    } else if (lastCell.getAttribute("data-available") === "true") {
+      lastCell.parentElement.classList.add("available");
+    } else if (lastCell.getAttribute("data-available") === "false") {
+      lastCell.parentElement.classList.add("unavailable");
+    }
+  }
 }
